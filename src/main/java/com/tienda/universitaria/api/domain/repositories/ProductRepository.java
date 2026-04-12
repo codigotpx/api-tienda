523ceaf6f65152d9437533912b80282eb60c37c2
cerpa <cerpacamilo3@gmail.com>
2026-04-12 13:23:32 -0500

    Fix a repository for return page when are necesary

--- src/main/java/com/tienda/universitaria/api/domain/repositories/ProductRepository.java
+++ src/main/java/com/tienda/universitaria/api/domain/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.tienda.universitaria.api.domain.repositories;
 
 import com.tienda.universitaria.api.domain.entities.Category;
 import com.tienda.universitaria.api.domain.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -21,8 +23,15 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     List<Product> findByActiveTrue();
 
+    Page<Product> findByActiveTrue(Pageable pageable);
+
+    Page<Product> findByCategoryId(UUID categoryId, Pageable pageable);
+
     List<Product> findAllByOrderByPriceAsc();
 
     @Query("SELECT p FROM Product p WHERE p.inventory.availableStock <= p.inventory.minimumStock")
     List<Product> findProductsWithLowStock();
+
+    @Query("SELECT p FROM Product p WHERE p.inventory.availableStock <= p.inventory.minimumStock")
+    Page<Product> findProductsWithLowStock(Pageable pageable);
 }
